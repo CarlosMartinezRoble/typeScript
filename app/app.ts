6c773dd58bd7650296d8282a568fc632eefa67ca
Carlos Martinez <carlos.martinez.robles@the-cocktail.com>
2022-09-29 13:48:32 +0200

    tsconfig.base.json update

--- app/app.ts
+++ app/app.ts
@@ -3,4 +3,12 @@ function startGame() {
   var messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to ultiMath! Satarting new Game..';
 }
+
+function logPlayer ( name) {
+  console.log(`New game starting for player : ${name}`);
+  
+};
+
 document.getElementById('startGame')?.addEventListener('click', startGame);
+
+
