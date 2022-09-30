3951006b53c1fa6e59bd9df5c6c6900c8a68f5dc
Carlos Martinez <carlos.martinez.robles@the-cocktail.com>
2022-09-30 13:57:20 +0200

    type custom

--- app/app.ts
+++ app/app.ts
@@ -1,20 +1,67 @@
 function startGame() {
   //stating a new game
   
-  let playerName: string = 'Audrey';
+  let playerName : string | undefined = getInputValue ('playerName');
   logPlayer( playerName);
 
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to ultiMath! Satarting new Game..';
+  postScore(80, playerName);
+  postScore(-5, playerName);
 }
 
-function logPlayer ( name : string ): void{
-  console.log(`New game starting for player : ${name}`);
+function logPlayer ( name : string ='MultiMath Player' ): void {
+  console.log(`New game starting for player : ${name}`);  
+}
+
+function getInputValue( elementID: string ): string | undefined {
   
-};
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  }
+  else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score : number, playerName: string = 'MultiMath Player') : void {
+ 
+ let logger : (value : string) => void;
+
+ if (score < 0) {
+  logger = logError;
+ }
+ else {
+  logger = logMessage;
+ }
+
+ const scoreElement : HTMLElement | null = document.getElementById( 'postedScores');
+ scoreElement!.innerText = `${score} - ${playerName}`;
+
+ logger(`Score: ${score}`);
+}
 
 document.getElementById('startGame')?.addEventListener('click', startGame);
 
+const logMessage = (message : string) => console.log(message);
+
+<<<<<<< HEAD
 
 
+=======
+function logError (err : string) : void {
+  console.error( err);
+}
 
+let myResult: Result = {
+  playerName: "",
+  score: 0,
+  problemCount: 0,
+  factor: 0
+};
+
+let player : Person = {
+  name: "Daniel",
+  formatName: () => 'Dan'
+  }; 
+>>>>>>> e86840e (type custom)
