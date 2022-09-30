e86840ee423ae133b9909c9e8bdf970c83d14661
Carlos Martinez <carlos.martinez.robles@the-cocktail.com>
2022-09-30 13:57:20 +0200

    type custom

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+entry : './js/app.js',
+  /*
+   * devtool : 'inline-source-map',
+   * module : {
+   * rules : [
+   *   {
+   *     test : /\.tsx?$/,
+   *     use : 'ts-loader',
+   *     exclude : /node_modules/,
+   *   }
+   * ]
+   * },
+   * resolve: {
+   *   extensions: [ '.tsx','.ts','.js']
+   * },
+   * output : {
+   *   fileName : 'bundle.js'
+   * },
+    */
+  devServer : {
+    inline : false
+  }
+};
